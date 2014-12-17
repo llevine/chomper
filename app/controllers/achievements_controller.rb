3b1e1843b13a86feb7b17d772de77a03eacaa62f
llevine <LaurenLevineDesigns@gmail.com>
2014-12-17 18:15:15 -0500

    fixing bugs starting over

--- app/controllers/achievements_controller.rb
+++
@@ -1,7 +0,0 @@
-class AchievementController < ApplicationController
-	def index
-	end
-
-	def show
-	end
-end
--- app/models/achievement.rb
+++
@@ -1,6 +0,0 @@
-class Achievement < ActiveRecord::Base
-	belongs_to :users
-
-	validates_presence_of :badge
-	validates_numericality_of :highscore, :on => :create
-end
