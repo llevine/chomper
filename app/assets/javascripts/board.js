e1492e51e00b81b14accdd7a286e0d4caa232747
llevine <LaurenLevineDesigns@gmail.com>
2014-12-24 03:45:33 -0500

    board.gettriviaquestion now complete

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -8,37 +8,44 @@ Board.prototype.newGame = function(){
 	this.answers = [];
 	var qAndA = {};
 
-	for (var i=0; i<test.length; i++){
+	for (var i=0; i<triviaConnector.length; i++){
 		// creates a new cell object and sets value of that object to a random trivia answer
 		this.answers.push(new Cell);
-		this.answers[i].setState(test[i]['answer']);
+		this.answers[i].setState(triviaConnector[i]['answer']);
 
 		// adds a question an answer object to an array
-		qAndA = {question: test[i]['question'], answer: test[i]['answer']};		
+		qAndA = {question: triviaConnector[i]['question'], answer: triviaConnector[i]['answer']};		
 		this.trivia.push(qAndA);
 	}
 }
 
 Board.prototype.getTriviaQuestion = function(){
-	var index = Math.floor(Math.random() * (this.trivia.length + 1))
-	var triviaquestion = this.trivia[index];
-	this.solved.push(this.trivia.splice(index, 1));
+	// console.log(this.trivia.length);
+	if (this.trivia.length > 0){
+		// returns a random number
+		var index = Math.floor(Math.random() * (this.trivia.length));
+		// returns a random trivia question at the random index number chosen above
+		var triviaquestion = this.trivia[index];
+		// removes the trivia question from the array and saves it to the variable removed
+		var removed = this.trivia.splice(index, 1);
+		// adds the removed question to a solved array
+		this.solved.push(removed[0]);
+	}
+	else {
+		// maybe move the above if statement to the game page as a game over scenerio checker...
+		console.log('game over');
+	}
 	console.log(triviaquestion['question']);
 	console.log(this.solved);
 	console.log(this.trivia);
 }
 
-Board.prototype.makePlay = function(){
-
-	console.log(this.question);
+Board.prototype.makePlay = function(chosenCell){
 
 }
 
 Board.prototype.checkWin = function(){
-	if (this.question.length === 0) {
-		console.log("gameover!");
-	}
-	else {
-		console.log("there are still active cells");
+	if (this.trivia.length === 0){
+		return true;
 	}
 }
--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -1,5 +1,15 @@
-function Game(){
-	this.gameOver = false;
-	
-}
+// gameOver = false;
 
+// var game = {
+// 	makeBoard: function() {
+// 		this.board = new Board();
+// 	},
+// 	start: function() {
+// 		this.makeBoard();
+// 		this.board.startGame();
+// 	},
+// 	play: function(position) {
+// 		this.board.makePlay(position);
+// 		var playableCells = this.board.boardView.map(function(cell) { return cell.playable(); });
+// 	}
+// };
